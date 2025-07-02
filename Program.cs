f173e5ba3ae891cb4b98c4d5eb85ed93d51d0ca3
i3ym <20320769+i3ym@users.noreply.github.com>
2025-07-03 06:48:56 +0700

    Add support for slash commands

--- Program.cs
+++ Program.cs
@@ -1,230 +1,257 @@
-﻿using System.Diagnostics;
-using System.Globalization;
+﻿using System.Collections;
+using System.Diagnostics;
 using Discord;
+using Discord.Audio;
+using Discord.Net;
 using Discord.WebSocket;
-
-var ffmpeg = File.Exists("ffmpeg") ? "./ffmpeg" : "ffmpeg";
-var currentFile = "current";
-
-var streamList = new MultiStreamWriter();
+using Newtonsoft.Json;
 
 using var discord = new DiscordSocketClient(new DiscordSocketConfig() { GatewayIntents = GatewayIntents.GuildVoiceStates | GatewayIntents.Guilds });
 discord.Log += async (log) =>
-{
     Console.WriteLine($"[{DateTimeOffset.Now}] [{log.Severity}] {(object) log.Exception ?? log.Message}");
+
+var player = new SongStreamSourceFromDirectory("source");
+var targets = new DiscordTargets(discord);
+player.Targets = targets;
+
+discord.Ready += async () => new Thread(async () => await player.Run()) { IsBackground = false }.Start();
+discord.Ready += async () => new Thread(async () => await targets.Start()) { IsBackground = false }.Start();
+discord.Ready += async () => new Thread(async () => await RunRpcSenderLoop()) { IsBackground = false }.Start();
+discord.Ready += async () =>
+{
+    var command = new SlashCommandBuilder()
+        .WithName("mgplay")
+        .WithDescription("makss gminag play")
+        .AddOption("search", ApplicationCommandOptionType.String, "search string", true);
+
+    try
+    {
+        await discord.CreateGlobalApplicationCommandAsync(command.Build());
+    }
+    catch (HttpException exception)
+    {
+        var json = JsonConvert.SerializeObject(exception.Errors, Formatting.Indented);
+        Console.WriteLine(json);
+    }
 };
 
-discord.Ready += async () => _ = Start("source");
+discord.SlashCommandExecuted += async command =>
+{
+    if (command.CommandName == "mgplay")
+    {
+        var regex = (string) command.Data.Options.First(c => c.Name == "search").Value;
+
+        var dic = Directory.GetFiles("source", $"*{regex}*");
+        if (dic.Length == 0)
+        {
+            await command.RespondAsync("no");
+            return;
+        }
+
+        var next = dic[0];
+        await command.RespondAsync($"ok {Path.GetFileNameWithoutExtension(next)}");
+        player.ForcePlayFile(next);
+    }
+};
 
 await discord.LoginAsync(Discord.TokenType.Bot, File.ReadAllText("discordkey"));
 await discord.StartAsync();
 
 await Task.Delay(-1);
+// GC.KeepAlive(interactions);
 
 
-async Task Start(string directory)
+async Task RunRpcSenderLoop()
 {
-    Queue<string> getFiles() => new(Directory.GetFiles(directory).OrderBy(_ => Guid.NewGuid()));
-
-    using var guild = discord.GetGuild(1053774759244083280);
-    var voiceChannel = guild.GetVoiceChannel(1386284245546307675);
-    string? currentPlayingPath = null;
-
-    Task sendRpc() => discord.SetActivityAsync(new Game(Path.GetFileName(currentPlayingPath).Split('[', StringSplitOptions.TrimEntries)[0], ActivityType.Listening));
-    new Thread(async () =>
-    {
-        while (true)
-        {
-            if (currentPlayingPath is not null)
-                await sendRpc();
-
-            await Task.Delay(10_000);
-        }
-    })
-    { IsBackground = true }.Start();
-
-    DateTimeOffset? songStartedTime = null;
+    Task sendRpc(string text) => discord.SetActivityAsync(new Game(Path.GetFileName(text).Split('[', StringSplitOptions.TrimEntries)[0], ActivityType.Listening));
 
     while (true)
     {
-        try
+        if (player.CurrentlyPlaying is not null)
+            await sendRpc(player.CurrentlyPlaying);
+
+        await Task.Delay(10_000);
+    }
+}
+
+interface ISongStreamTarget
+{
+    Task Write(ReadOnlyMemory<byte> data);
+    Task Flush();
+}
+abstract class SongStreamSource
+{
+    public abstract string? CurrentlyPlaying { get; }
+
+    protected static async Task WriteSongTo(string path, IEnumerable<ISongStreamTarget> targets, CancellationToken cancellation)
+    {
+        var ffmpeg = File.Exists("ffmpeg") ? "./ffmpeg" : "ffmpeg";
+        var psi = new ProcessStartInfo(ffmpeg)
         {
-            var bucket = new Queue<string>();
-            var startingTimeSec = 0;
-            if (File.Exists(currentFile))
+            RedirectStandardOutput = true,
+            ArgumentList =
             {
-                var data = File.ReadAllLines(currentFile);
-                startingTimeSec = int.Parse(data[1], CultureInfo.InvariantCulture);
+                "-hide_banner",
+                "-i", path,
+                "-ac", "2",
+                "-ar", "48000",
+                "-f", "s16le",
+                "pipe:1",
+            },
+        };
+        Console.WriteLine($"Launching {psi.FileName} {string.Join(' ', psi.ArgumentList)}");
 
-                bucket.Enqueue(data[0]);
-                File.Delete(currentFile);
-                Console.WriteLine($"Starting {data[0]} at {TimeSpan.FromSeconds(startingTimeSec)}");
-            }
+        using var proc = Process.Start(psi)!;
+        using var ffmpegOutput = proc.StandardOutput.BaseStream;
+        cancellation.Register(proc.Kill);
 
-            using var audioClient = await voiceChannel.ConnectAsync(selfDeaf: true);
-            using var audioStream = audioClient.CreatePCMStream(Discord.Audio.AudioApplication.Music, 128 * 1024);
+        var buffer = new byte[1024 * 128];
+        while (true)
+        {
+            if (cancellation.IsCancellationRequested)
+                break;
 
-            while (true)
-            {
-                if (bucket.Count == 0)
-                    bucket = getFiles();
+            var read = await ffmpegOutput.ReadAsync(buffer, cancellation);
+            if (read < 1) break;
 
-                var path = bucket.Dequeue();
-                currentPlayingPath = path;
-                songStartedTime = DateTimeOffset.Now;
+            while (!targets.Any())
+                await Task.Delay(100, cancellation);
 
-                _ = sendRpc();
-                Console.WriteLine($"Playing {path}");
-                await ConvertToPcm(path, audioStream, startingTimeSec);
-
-                currentPlayingPath = null;
-                startingTimeSec = 0;
-            }
+            await Task.WhenAll(targets.Select(c => c.Write(buffer.AsMemory(0, read))));
         }
-        catch (Exception ex)
+
+        if (cancellation.IsCancellationRequested)
+            return;
+
+        await proc.WaitForExitAsync(cancellation);
+        await Task.WhenAll(targets.Select(c => c.Flush()));
+    }
+}
+class SongStreamSourceFromDirectory : SongStreamSource
+{
+    public IEnumerable<ISongStreamTarget> Targets { get; set; } = [];
+    readonly string DirectoryPath;
+    CancellationTokenSource? Cancellation;
+
+    string? _CurrentlyPlaying;
+    public override string? CurrentlyPlaying => _CurrentlyPlaying;
+
+    string? NextToPlay;
+
+    public SongStreamSourceFromDirectory(string directory) => DirectoryPath = directory;
+
+    public void ForcePlayFile(string path)
+    {
+        NextToPlay = path;
+        Cancellation?.Cancel();
+    }
+
+    public async Task Run()
+    {
+        Queue<string> getFiles() => new(Directory.GetFiles(DirectoryPath).OrderBy(_ => Guid.NewGuid()));
+
+        while (true)
         {
-            Console.WriteLine(ex);
-            Console.WriteLine("RECONNECTING...");
-
-            if (currentPlayingPath is not null && songStartedTime is { } sst)
+            try
             {
-                var diff = (int) (DateTimeOffset.Now - sst).TotalSeconds;
-                File.WriteAllLines(currentFile, [currentPlayingPath, diff.ToString(CultureInfo.InvariantCulture)]);
+                var bucket = new Queue<string>();
 
-                Console.WriteLine($"Will restart {currentPlayingPath} at {TimeSpan.FromSeconds(diff)}");
+                while (true)
+                {
+                    if (bucket.Count == 0)
+                        bucket = getFiles();
+
+                    var path = NextToPlay ?? bucket.Dequeue();
+                    NextToPlay = null;
+                    _CurrentlyPlaying = path;
+
+                    Console.WriteLine($"Playing {path}");
+                    Cancellation = new();
+                    await WriteSongTo(path, Targets, Cancellation.Token);
+
+                    _CurrentlyPlaying = null;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine("Restarting?...");
             }
         }
     }
 }
 
-async Task ConvertToPcm(string path, Stream target, int startingTimeSec = 0)
+class DiscordTargets : IEnumerable<ISongStreamTarget>
 {
-    var psi = new ProcessStartInfo(ffmpeg)
+    public Dictionary<ulong, ISongStreamTarget> Targets { get; } = [];
+    readonly DiscordSocketClient Client;
+
+    public DiscordTargets(DiscordSocketClient client) => Client = client;
+
+    public async Task Start()
     {
-        RedirectStandardOutput = true,
-        ArgumentList =
-        {
-            "-hide_banner",
-            "-i", path,
-            "-ss", startingTimeSec.ToString(CultureInfo.InvariantCulture),
-            "-ac", "2",
-            "-ar", "48000",
-            "-f", "s16le",
-            "pipe:1",
-        },
-    };
-    Console.WriteLine($"Launching {psi.FileName} {string.Join(' ', psi.ArgumentList)}");
+        var toConnect = new Dictionary<ulong, ulong>() { [1053774759244083280] = 1386284245546307675 };
+        foreach (var (g, v) in toConnect)
+            await Connect(g, v);
+    }
+    async Task Connect(ulong guildId, ulong channelId)
+    {
+        var guild = Client.GetGuild(guildId);
+        var channel = guild.GetVoiceChannel(channelId);
 
-    using var proc = Process.Start(psi)!;
-    using var ffmpegOutput = proc.StandardOutput.BaseStream;
+        Targets[channelId] = new VoiceChannelState(this, channel);
+    }
 
-    await ffmpegOutput.CopyToAsync(target);
-    await proc.WaitForExitAsync();
-    await target.FlushAsync();
+    public IEnumerator<ISongStreamTarget> GetEnumerator() => ((IReadOnlyDictionary<ulong, ISongStreamTarget>) Targets).Values.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 }
-
-
-class VoiceState
+class VoiceChannelState : ISongStreamTarget, IAsyncDisposable
 {
-    public MultiStreamWriter? Writer { get; }
+    readonly DiscordTargets Targets;
+    readonly SocketVoiceChannel Channel;
+    AudioOutStream? AudioStream;
 
-    public async Task Connect(SocketVoiceChannel channel)
+    public VoiceChannelState(DiscordTargets targets, SocketVoiceChannel channel)
     {
-
-    }
-}
-
-
-[Obsolete]
-class MultiStreamWriter : Stream
-{
-    readonly List<VoiceState> Streams = [];
-    readonly Lock Lock = new();
-
-    public void AddStream(VoiceState stream)
-    {
-        if (!Streams.Contains(stream))
-            Streams.Add(stream);
-    }
-    public void RemoveStream(VoiceState stream) => Streams.Remove(stream);
-
-    public override bool CanRead => false;
-    public override bool CanSeek => false;
-    public override bool CanWrite => true;
-    public override long Length => throw new NotSupportedException();
-    public override long Position
-    {
-        get => throw new NotSupportedException();
-        set => throw new NotSupportedException();
+        Targets = targets;
+        Channel = channel;
     }
 
-    public override void Flush()
+    public async Task Write(ReadOnlyMemory<byte> data)
     {
-        foreach (var stream in Streams)
-            stream.Writer?.Flush();
+        if (AudioStream is null)
+            await Reconnect();
+
+        if (AudioStream is null) return;
+
+        try { await AudioStream.WriteAsync(data); }
+        catch { await Reconnect(); }
+    }
+    public async Task Flush()
+    {
+        if (AudioStream is null)
+            await Reconnect();
+
+        if (AudioStream is null) return;
+
+        try { await AudioStream.FlushAsync(); }
+        catch { await Reconnect(); }
     }
 
-    public override Task FlushAsync(CancellationToken cancellationToken) =>
-        Task.WhenAll(Streams.Select(c => c.Writer?.FlushAsync(cancellationToken)).Where(c => c is not null).ToArray()!);
-
-    public override int Read(byte[] buffer, int offset, int count) => throw new NotSupportedException();
-    public override long Seek(long offset, SeekOrigin origin) => throw new NotSupportedException();
-    public override void SetLength(long value) => throw new NotSupportedException();
-
-    public async ValueTask WriteAsync(ReadOnlyMemory<byte> bytes)
+    async Task Reconnect()
     {
-        var streams = Streams.ToArray();
-        foreach (var stream in streams)
-            await stream.WriteAsync(bytes);
-    }
-    public override void Write(ReadOnlySpan<byte> bytes)
-    {
-        lock (Lock)
-        {
-            foreach (var stream in Streams)
-            {
-                stream.Write(bytes);
-            }
-        }
-    }
-    public override void Write(byte[] buffer, int offset, int count)
-    {
-        if (buffer == null) throw new ArgumentNullException(nameof(buffer));
-        if (offset < 0 || count < 0 || (offset + count) > buffer.Length)
-            throw new ArgumentOutOfRangeException();
-
-        lock (Lock)
-        {
-            foreach (var stream in Streams)
-            {
-                stream.Write(buffer, offset, count);
-            }
-        }
+        var audioClient = await Channel.ConnectAsync(selfDeaf: true);
+        AudioStream = audioClient.CreatePCMStream(Discord.Audio.AudioApplication.Music, 128 * 1024);
     }
 
-    public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    public async ValueTask DisposeAsync()
     {
-        if (buffer == null) throw new ArgumentNullException(nameof(buffer));
-        if (offset < 0 || count < 0 || (offset + count) > buffer.Length)
-            throw new ArgumentOutOfRangeException();
+        Targets.Targets.Remove(Channel.Id);
 
-        List<Task> tasks;
-        lock (Lock)
-        {
-            tasks = new List<Task>();
-            foreach (var stream in Streams)
-            {
-                tasks.Add(stream.WriteAsync(buffer, offset, count, cancellationToken));
-            }
-        }
-        await Task.WhenAll(tasks).ConfigureAwait(false);
-    }
+        try { AudioStream?.Dispose(); }
+        catch { }
 
-    protected override void Dispose(bool disposing)
-    {
-        if (disposing)
-            Streams.Clear();
-
-        base.Dispose(disposing);
+        try { await Channel.DisconnectAsync(); }
+        catch { }
     }
 }
